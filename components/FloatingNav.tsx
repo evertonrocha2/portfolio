3c7a4f30d8d4591d9c65f181f097d2171019d937
evertonrocha2 <evertondarocha2018@gmail.com>
2025-02-23 17:11:24 -0300

    Last Changes

--- components/FloatingNav.tsx
+++ components/FloatingNav.tsx
@@ -4,7 +4,7 @@ const FloatingNav = () => {
   return (
     <div>
       <header>
-        <nav className="fixed w-96 rounded-full geist mx-auto top-0 left-0 right-0 bg-black-100 z-50 border flex items-center gap-8 justify-center p-4 my-8 border-zinc-800 sm:px-10 md:px-20 lg:px-28 xl:px-36">
+        <nav className="fixed w-64 md:w-96 rounded-full geist mx-auto top-0 left-0 right-0 bg-black-100 z-50 border flex items-center gap-8 justify-center p-4 my-8 border-zinc-800 sm:px-10 md:px-20 lg:px-28 xl:px-36">
           <div className="flex items-center gap-4">
             <a
               href="/"
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,14 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import FloatingNav from "../../../components/FloatingNav";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-  const [nome, setNome] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [mensagem, setMensagem] = React.useState("");
-  const [isShow, setIsShow] = React.useState(false);
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [mensagem, setMensagem] = useState("");
+  const [isShow, setIsShow] = useState(false);
+
   const activeConffetion = () => {
     setIsShow(true);
     setTimeout(() => {
@@ -14,14 +16,37 @@ const Contact = () => {
     }, 1800);
   };
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+
+    const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!;
+    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!;
+    const userID = process.env.NEXT_PUBLIC_EMAILJS_USER_ID!;
+
+    const templateParams = {
+      from_name: nome,
+      from_email: email,
+      message: mensagem,
+    };
+
+    emailjs
+      .send(serviceID, templateID, templateParams, userID)
+      .then(() => {
+        alert("E-mail enviado com sucesso!");
+        setNome("");
+        setEmail("");
+        setMensagem("");
+        activeConffetion();
+      })
+      .catch((error: any) => {
+        console.error("Erro ao enviar o e-mail:", error);
+      });
   };
 
   return (
     <div className="bg-black-100">
       <FloatingNav />
-      <div className="w-[20%] mx-auto geist flex flex-col h-screen relative justify-center">
+      <div className="md:w-[40%] sm:w-[60%] w-[80%] mx-auto geist flex flex-col h-screen relative justify-center">
         <div
           className={`absolute ${
             isShow ? "block" : "hidden"
@@ -29,7 +54,7 @@ const Contact = () => {
         >
           <img src="/confetti.gif" alt="Confetti" className="w-full h-full" />
         </div>
-        <h2 className="text-5xl text-center tracking-tighter">
+        <h2 className="text-3xl md:text-3xl text-center tracking-tighter">
           Entre em contato comigo! 😄
         </h2>
         <form onSubmit={handleSubmit}>
@@ -40,6 +65,9 @@ const Contact = () => {
               type="text"
               id="nome"
               name="nome"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              required
             />
           </div>
           <div className="flex flex-col gap-1 my-4">
@@ -49,6 +77,9 @@ const Contact = () => {
               type="email"
               id="email"
               name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
             />
           </div>
           <div className="flex flex-col gap-1 my-4">
@@ -58,10 +89,12 @@ const Contact = () => {
               placeholder="Exemplo: Olá, gostaria de te contratar 💖"
               className="px-4 bg-black-100 border-[0.2px] border-zinc-950 py-2 rounded-xl text-sm"
               name="mensagem"
+              value={mensagem}
+              onChange={(e) => setMensagem(e.target.value)}
+              required
             ></textarea>
           </div>
           <button
-            onClick={activeConffetion}
             className="bg-blue-500 px-4 py-2 rounded-xl text-center w-full hover:bg-blue-500/80 transition-colors duration-300"
             type="submit"
           >
