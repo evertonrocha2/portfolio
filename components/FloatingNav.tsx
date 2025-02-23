cb544b6e468e3488e08441946e624a775c806046
evertonrocha2 <evertondarocha2018@gmail.com>
2025-02-23 20:32:39 -0300

    Navigation bar adjustments

--- components/FloatingNav.tsx
+++ components/FloatingNav.tsx
@@ -4,8 +4,8 @@ const FloatingNav = () => {
   return (
     <div>
       <header>
-        <nav className="fixed w-64 md:w-96 rounded-full geist mx-auto top-0 left-0 right-0 bg-black-100 z-50 border flex items-center gap-8 justify-center p-4 my-8 border-zinc-800 sm:px-10 md:px-20 lg:px-28 xl:px-36">
-          <div className="flex items-center gap-4">
+        <nav className="fixed w-64 md:w-64 rounded-full geist mx-auto top-0 left-0 right-0 bg-black-100 z-50 border flex items-center gap-8 justify-center p-4 my-8 border-zinc-800 sm:px-10 md:px-20 lg:px-28 xl:px-36">
+          <div className="flex items-center gap-4 justify-center">
             <a
               href="/"
               className="text-md font-regular text-white hover:text-white/70 transition-colors duration-200"
@@ -13,7 +13,7 @@ const FloatingNav = () => {
               Início
             </a>
           </div>
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-4 justify-center">
             <a
               href="/contact"
               target="_blank"
